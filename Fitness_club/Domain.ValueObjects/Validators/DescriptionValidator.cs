f9744dd5a3dc50fd6ed0b63a4862dcaad4b1dd25
MedvedevSer <ser.medvedev034@gmail.com>
2026-03-30 23:08:44 +0700

    Revert "#11 Исправлен DescriptionValidator: добавлены информативные сообщения об ошибках"
    This reverts commit 907b945d4fdf67f7e690429a07abbd656b9dd705.

--- Fitness_club/Domain.ValueObjects/Validators/DescriptionValidator.cs
+++ Fitness_club/Domain.ValueObjects/Validators/DescriptionValidator.cs
@@ -5,21 +5,12 @@ namespace Domain.ValueObjects.Validators;
 
 public class DescriptionValidator : IValidator<string>
 {
-    public static int MAX_LENGTH => 500;
-
     public void Validate(string value)
     {
         if (string.IsNullOrWhiteSpace(value))
-        {
             throw new ArgumentNullOrWhiteSpaceException(nameof(value));
-        }
 
-        if (value.Length > MAX_LENGTH)
-        {
-            throw new ArgumentLongValueException(
-                paramName: nameof(value),
-                value: value,
-                maxLength: MAX_LENGTH);
-        }
+        if (value.Length > 500)
+            throw new ArgumentLongValueException(nameof(value), value, 500);
     }
 }
