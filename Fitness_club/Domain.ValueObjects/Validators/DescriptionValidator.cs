2c0b86574ca6002d99f42cc29ae739f0a3dd6fc6
MedvedevSer <ser.medvedev034@gmail.com>
2026-03-30 23:07:24 +0700

    #2 Исправлен DescriptionValidator: добавлены информативные сообщения об ошибках

--- Fitness_club/Domain.ValueObjects/Validators/DescriptionValidator.cs
+++ Fitness_club/Domain.ValueObjects/Validators/DescriptionValidator.cs
@@ -5,12 +5,21 @@ namespace Domain.ValueObjects.Validators;
 
 public class DescriptionValidator : IValidator<string>
 {
+    public static int MAX_LENGTH => 500;
+
     public void Validate(string value)
     {
         if (string.IsNullOrWhiteSpace(value))
+        {
             throw new ArgumentNullOrWhiteSpaceException(nameof(value));
+        }
 
-        if (value.Length > 500)
-            throw new ArgumentLongValueException(nameof(value), value, 500);
+        if (value.Length > MAX_LENGTH)
+        {
+            throw new ArgumentLongValueException(
+                paramName: nameof(value),
+                value: value,
+                maxLength: MAX_LENGTH);
+        }
     }
 }
